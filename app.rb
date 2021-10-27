14e942a918cc92880a299031e3be6b17b3352cbc
Jenaro Garcia <jjegarcia@hotmail.com>
2021-10-28 00:11:46 +0100

    replaced hard coded names with session names

--- app.rb
+++ app.rb
@@ -2,19 +2,23 @@ require 'sinatra/base'
 require 'sinatra/reloader'
 
 class Battle < Sinatra::Base
-  configure :development do
-    register Sinatra::Reloader
-  end
+  enable :sessions
 
   get '/' do
     erb :index
   end
 
-  post '/names' do
-    @player_1_name = params[:player_1_name]
-    @player_2_name = params[:player_2_name]
+  get '/play' do
+    @player_1_name = session[:player_1_name]
+    @player_2_name = session[:player_2_name]
     erb :play
   end
 
-  run! if app_file == $0
+  post '/names' do
+    session[:player_1_name] = params[:player_1_name]
+    session[:player_2_name] = params[:player_2_name]
+    redirect '/play'
+  end
+  # run! if app_file == $0
+
 end
