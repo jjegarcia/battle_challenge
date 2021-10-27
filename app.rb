3db994d419a0bf494646cc22e74a547c24136bc7
Jenaro Garcia <jjegarcia@hotmail.com>
2021-10-28 00:00:38 +0100

    battle names displayed

--- app.rb
+++ app.rb
@@ -7,8 +7,14 @@ class Battle < Sinatra::Base
   end
 
   get '/' do
-    'Testing infrastructure working!'
+    erb :index
   end
-  # start the server if ruby file executed directly
+
+  post '/names' do
+    @player_1_name = params[:player_1_name]
+    @player_2_name = params[:player_2_name]
+    erb :play
+  end
+
   run! if app_file == $0
 end
