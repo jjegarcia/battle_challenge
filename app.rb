abae0b0cb0a4ebb741b4a08b3b59dfd08efcd565
Jenaro Garcia <jjegarcia@hotmail.com>
2021-10-28 22:47:40 +0100

    hit points implemented with extraction of player

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require 'sinatra/base'
 require 'sinatra/reloader'
+require_relative './lib/player'
 
 class Battle < Sinatra::Base
   enable :sessions
@@ -15,14 +16,15 @@ class Battle < Sinatra::Base
   end
 
   get '/play' do
-    @player_1_name = $player_1.name
-    @player_2_name = $player_2.name
+    @player1 = $player_1
+    @player2 = $player_2
     erb :play
   end
 
   get '/attack' do
-    @player_1_name = $player_1.name
-    @player_2_name = $player_2.name
+    @player1 = $player_1
+    @player2 = $player_2
+    @player2.receive_damage
     erb :attack
   end
 end
--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -4,4 +4,10 @@ feature 'Attacking' do
     click_button 'Attack'
     expect(page).to have_content 'Charlotte attacked Mittens'
   end
+  scenario 'I want my attack to reduce Player 2\'s HP by 10' do
+    sign_in_and_play
+    click_button 'Attack'
+    expect(page).not_to have_content 'Mittens: 60HP'
+    expect(page).to have_content 'Mittens: 50HP'
+  end
 end
