1b4b4c523c3c7fa5059c7b2d3047b787982d8b31
Jenaro Garcia <jjegarcia@hotmail.com>
2021-10-28 20:45:55 +0100

    WIP hit_points uncertain

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -1,7 +1,9 @@
 feature 'Attacking' do
-  scenario 'attack Player 2' do
+
+  scenario 'reduce Player 2 HP by 10' do
     sign_in_and_play
     click_button 'Attack'
-    expect(page).to have_content 'Charlotte attacked Mittens'
+    expect(page).not_to have_content 'Mittens: 60HP'
+    expect(page).to have_content 'Mittens: 50HP'
   end
 end
